a08061f570bca799e81164adabdc1eb4bff8b973
Ruan Nicolini <ruannicolini@gmail.com>
2015-04-22 19:44:56 -0300

    Implementação das classes Locação e Pagamento

--- src/java/model/domain/Pagamento.java
+++ src/java/model/domain/Pagamento.java
@@ -3,13 +3,71 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 /**
  *
  * @author Ruan
  */
+@Entity
 public class Pagamento {
+
+    @Id
+    @GeneratedValue
+    private long idPagamento;
+    private String data;
+    private float valor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<Locacao> locacoes = new HashSet<Locacao>();
+
+    public Pagamento() {
+    }
+
+    public Pagamento(String data, float valor) {
+        this.data = data;
+        this.valor = valor;
+    }
+
+    public long getIdPagamento() {
+        return idPagamento;
+    }
+
+    public void setIdPagamento(long idPagamento) {
+        this.idPagamento = idPagamento;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Set<Locacao> getLocacoes() {
+        return locacoes;
+    }
+
+    public void setLocacoes(Set<Locacao> locacoes) {
+        this.locacoes = locacoes;
+    }
+    
     
 }
